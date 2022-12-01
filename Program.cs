a3d4e8c4404459bde656b685aa7029c1be389c17
CoffeeMan <68038942+CoffeeMan240@users.noreply.github.com>
2022-12-01 10:21:05 -0700

    IT'S TIME

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
 
 namespace CANYUOUPOSTONGITHUBNOW
 {
@@ -7,6 +11,24 @@ namespace CANYUOUPOSTONGITHUBNOW
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            HttpListener server = new HttpListener();
+            server.Prefixes.Add("*:6969");
+            for(; ; )
+            {
+                server.Start();
+                Console.WriteLine("Abuseing Github for RecNoot might happen lol");
+                var ctx = server.GetContext();
+                string rawurl = ctx.Request.RawUrl;
+                string r = "HELLO! HOW ARE YOU?";
+                Console.WriteLine("Requested! " + rawurl);
+                byte[] send = Encoding.ASCII.GetBytes(r);
+                ctx.Response.ContentLength64 = send.Length;
+                Stream output = ctx.Response.OutputStream;
+                output.Write(send, 0, send.Length);
+                Thread.Sleep(100);
+                server.Close();
+
+            }
         }
     }
 }
